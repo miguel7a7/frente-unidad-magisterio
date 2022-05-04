f5ea38ed0ca80b0e045c955d82a894a28906301d
miguel angel <mickmiguel05@gmail.com>
2022-05-04 14:51:56 -0400

    adicion de usuario en Nosotros

--- src/components/principal/nosotros/NosotrosScreen.js
+++ src/components/principal/nosotros/NosotrosScreen.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import { Ejecutivo } from './Ejecutivo';
+import React from "react";
+import { Ejecutivo } from "./Ejecutivo";
 
-import './notosotros.css';
+import "./notosotros.css";
 
 export const NosotrosScreen = () => {
   const ejecutivos = [
     {
       id: 1,
-      nombre: 'Sonia Tellez Chambi',
-      cargo: 'Secretario Ejecutivo',
-      telefono: '68166974',
-      email: 'sonia.tellez@gmail.com',
-      namefoto: 'usuario-3',
+      nombre: "Sonia Tellez Chambi",
+      cargo: "Secretario Ejecutivo",
+      telefono: "68166974",
+      email: "sonia.tellez@gmail.com",
+      namefoto: "usuario-3",
     },
     {
       id: 2,
-      nombre: 'Roman Quispe Mamani',
-      cargo: 'Secretario Ejecutivo',
-      telefono: '69775258',
-      email: 'roman.quispe@gmail.com',
-      namefoto: 'usuario-1',
+      nombre: "Roman Quispe Mamani",
+      cargo: "Secretario Ejecutivo",
+      telefono: "69775258",
+      email: "roman.quispe@gmail.com",
+      namefoto: "usuario-1",
     },
     {
       id: 3,
-      nombre: 'Maribel Castro Copa',
-      cargo: 'Secretario Ejecutivo',
-      telefono: '68166974',
-      email: 'sonia.tellez@gmail.com',
-      namefoto: 'usuario-3',
+      nombre: "Maribel Castro Copa",
+      cargo: "Secretario Ejecutivo",
+      telefono: "68166974",
+      email: "sonia.tellez@gmail.com",
+      namefoto: "usuario-3",
+    },
+    {
+      id: 4,
+      nombre: "Mario Callizara Paco",
+      cargo: "Secretario Ejecutivo",
+      telefono: "68166974",
+      email: "sonia.tellez@gmail.com",
+      namefoto: "usuario-3",
     },
   ];
 
